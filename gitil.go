80dabba54815f3fd4a2937fea175114084589d9f
Steven Braham <steven@braham.biz>
2017-05-03 21:18:08 +0200

    added merge branch function

--- gitil.go
+++ gitil.go
@@ -27,10 +27,33 @@ func main() {
 				fmt.Println("Cloned", c.Args().First())
 				return nil
 			},
+			Name:     "to-master",
+			Category: "Merge commands",
+			Usage:    "Merges the selected branch into master",
+			Action: func(c *cli.Context) error {
+				var branch = c.Args().First()
+				mergeBranch(branch, "master")
+				fmt.Println("Merged", branch, " into master")
+				return nil
+			},
 		}}
 	app.Run(os.Args)
 }
 
 func cloneRepository(url string) {
-	exec.Command("git", "clone", url).Output()
+	exec.Command("git", "clone", url)
+}
+
+//checkouts a branch
+func checkoutBranch(branch string) {
+	exec.Command("git", "checkout", branch)
+}
+
+//merges source into destination and checkouts source again
+func mergeBranch(source, destination string) {
+	exec.Command("git", "fetch", "--all")
+	checkoutBranch(destination)
+	exec.Command("git", "merge", source)
+	exec.Command("git", "push")
+	checkoutBranch(source)
 }
