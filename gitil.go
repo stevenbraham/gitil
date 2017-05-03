9408475548018cb5f9230f8f9925dbdc74fc3241
Steven Braham <steven@braham.biz>
2017-05-03 12:44:38 +0200

    clone repository poc

--- gitil.go
+++ gitil.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"gopkg.in/urfave/cli.v1"
+	"os"
+	"os/exec"
+)
+
 func main() {
+	//init app
+	app := cli.NewApp()
+	app.Name = "Gitil"
+	app.Version = "0.1.0"
+	app.Usage = "Gitil is a wrapper for git that has commands for common tasks"
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("boom! I say!")
+		return nil
+	}
+	app.Commands = []cli.Command{
+		{
+			Name:     "clone",
+			Category: "Init commands",
+			Usage:    "clones a repository",
+			Action: func(c *cli.Context) error {
+				exec.Command("git", "clone", c.Args().First()).Output()
+				fmt.Println("Cloned", c.Args().First())
+				return nil
+			},
+		}}
+	app.Run(os.Args)
 }
